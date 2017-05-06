79ece79b9add78f30a21b70fa8d8dd680e54bf92
Abhinav Sharma <abhinav.sharma@nuvoex.com>
2017-05-06 18:40:18 +0530

    Some more solutions

---
+++ drop_elements.js
@@ -0,0 +1,17 @@
+
+function dropElements(arr, func) {
+  // Drop them elements.
+  var len = arr.length;
+  for(var i = 0; i<len; i++){
+    if(func(arr[i])){
+      break;
+    }
+  }
+   while(i>0){
+    arr.shift();
+    i--;
+   }
+  return arr;
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; });
